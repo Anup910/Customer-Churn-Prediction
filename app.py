43831f60f23fcb4d7e3c58a572d7dd5a90891c8d
Anup Kumar Raj <anup.kumar.raj_ds24sp@praxistech.school>
2025-03-15 21:33:24 +0530

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1MdypSvOKEzG6jcLq0h6EZJaYbMkqb2l7
 """
 
-pip install streamlit pandas numpy scikit-learn xgboost matplotlib seaborn imbalanced-learn
-
 import streamlit as st
 import pickle
 import pandas as pd
@@ -39,5 +37,3 @@ input_df = pd.DataFrame([inputs])
 if st.button("Predict"):
     prediction = model.predict(input_df)
     st.write(f"Churn Prediction: {'Churn' if prediction[0] == 1 else 'Not Churn'}")
-
-!streamlit run app.py
